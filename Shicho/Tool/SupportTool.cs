2845473c6497232de0f01229f148d217e0bd2157
Nana Sakisaka <saki7@setna.io>
2018-04-29 01:02:17 +0900

    GUI WIP

--- Shicho/Tool/SupportTool.cs
+++ Shicho/Tool/SupportTool.cs
@@ -47,9 +47,17 @@ namespace Shicho.Tool
         public void OnGUI()
         {
             if (cfg_.IsVisible) {
-                var style = new GUIStyle(GUI.skin.window);
-                style.onNormal.background = null;
-
+                var style = new GUIStyle(GUI.skin.window) {
+                    fontSize = Defaults.TitleFontSize,
+                    onNormal = {
+                        background = null,
+                    },
+                    //padding = {
+                    //    top = 2,
+                    //    bottom = 4,
+                    //},
+                    stretchHeight = true,
+                };
                 cfg_.Rect = GUI.Window(ID, cfg_.Rect, DrawWindow, "Shicho", style);
             }
         }
@@ -62,12 +70,36 @@ namespace Shicho.Tool
 
         private void DrawWindow(int id)
         {
-            cfg_.Tab.Index = GUI.Toolbar(new Rect(0, HUnit, cfg_.Rect.width, HUnit), cfg_.Tab.Index, Tabs);
+            {
+                var style = new GUIStyle(GUI.skin.label) {
+                    fontSize = Defaults.TabFontSize,
+                    onNormal = {
+                        background = null,
+                    },
+                    margin = {
+                        left = 0,
+                        right = 0,
+                    },
+                    padding = {
+                        top = Defaults.TabFontSize / 3,
+                        left = 0,
+                        right = 0,
+                    },
+                    alignment = TextAnchor.UpperCenter,
+                    fixedHeight = Defaults.TabHeight,
+                };
+
+                cfg_.Tab.Index = GUI.Toolbar(new Rect(0, HUnit / 2, cfg_.Rect.width, HUnit), cfg_.Tab.Index, Tabs, style);
+            }
             var tabID = Tabs[cfg_.Tab.Index];
 
             switch (tabID) {
             case "Graphics":
                 GraphicsTab();
+                break;
+
+            case "Log":
+                LogTab();
                 break;
 
             default:
@@ -80,18 +112,47 @@ namespace Shicho.Tool
 
         private void GraphicsTab()
         {
-            GUI.BeginGroup(new Rect(0, 0, sliderWidth, 400));
+            var y = Defaults.Top;
+            {
+                var style = new GUIStyle(GUI.skin.box) {
+                    fontSize = Defaults.FontSize,
+                };
+                GUI.BeginGroup(new Rect(0, y, sliderWidth, 400), style);
+            }
             var shadowBiasMethods = new Dictionary<string, ShadowBiasMethod>() {
                 {"Default", new ShadowBiasMethod()},
                 {"Hybrid (Relight v2018.04)", new ShadowBiasMethod()},
             };
-            GUILayout.SelectionGrid(0, shadowBiasMethods.Keys.ToArray(), 1, "Toggle");
+
+            {
+                var style = new GUIStyle(GUI.skin.toggle) {
+                    fontSize = Defaults.FontSize,
+                };
+                GUILayout.SelectionGrid(0, shadowBiasMethods.Keys.ToArray(), 1, style);
+            }
+
             GUI.EndGroup();
+        }
+
+        private void LogTab()
+        {
+            GUI.Label(new Rect(0, HUnit * 4, cfg_.Rect.width, HUnit), "Not implemented");
         }
 
         readonly string[] Tabs = new[] {"Graphics", "Log"};
         const float HUnit = 24f;
         const int sliderWidth = 200;
+
+        static class Defaults
+        {
+            public const int FontSize = 16;
+
+            public const int TitleFontSize = 16;
+            public const int TabFontSize = 16;
+
+            public const float TabHeight = TabFontSize * 2.2f;
+            public const float Top = TitleFontSize * 1.1f + TabHeight;
+        }
 
         public static readonly Rect DefaultRect = new Rect(
             0f, 0f, 280f, 800f
