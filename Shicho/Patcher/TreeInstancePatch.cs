0706a910b2441e1c4ee1c85afc393cc93abec99d
Nana Sakisaka <saki7@setna.io>
2018-05-26 02:49:50 +0900

    Parklife compat

--- Shicho/Patcher/TreeInstancePatch.cs
+++ Shicho/Patcher/TreeInstancePatch.cs
@@ -21,6 +21,7 @@ namespace Shicho.Patcher.TreeInstancePatch
     using TreeInstance = Cities::TreeInstance;
     using System.Collections.Generic;
 
+
     [HarmonyPatch(typeof(TreeInstance))]
     [HarmonyPatch("RenderInstance")]
     [HarmonyPatch(new Type[] {
@@ -29,10 +30,11 @@ namespace Shicho.Patcher.TreeInstancePatch
         typeof(Vector3),
         typeof(float),
         typeof(float),
+        typeof(Vector4)
     })]
     class RenderInstance
     {
-        static bool Prefix(ref RenderManager.CameraInfo cameraInfo, ref TreeInfo info, Vector3 position, float scale, float brightness)
+        static bool Prefix(ref RenderManager.CameraInfo cameraInfo, ref TreeInfo info, Vector3 position, float scale, float brightness, Vector4 objectIndex)
         {
             if (info.m_prefabInitialized) {
                 if (cameraInfo == null || (UnityEngine.Object)info.m_lodMesh1 == (UnityEngine.Object)null || cameraInfo.CheckRenderDistance(position, info.m_lodRenderDistance)) {
@@ -44,7 +46,7 @@ namespace Shicho.Patcher.TreeInstancePatch
                     Quaternion rotation;
                     lock (App.Config.GraphicsLock) {
                         if (App.Config.Graphics.randomTrees) {
-#if false
+                            #if false
                             UInt32 randIdent =
                                 ((UInt32)info.GetInstanceID() << 16) |
                                 (((UInt32)(BitConverter.DoubleToInt64Bits(position.magnitude) >> 32)) & 0xFFFF)
@@ -53,7 +55,7 @@ namespace Shicho.Patcher.TreeInstancePatch
                             //Log.Debug($"tree rand: {Convert.ToString(randIdent, 2)}, {r.NextDouble() * 360}");
 
                             rotation = Quaternion.Euler(0, (float)r.NextDouble() * 360, 0);
-#endif
+                            #endif
 
                             // same angle as in "Random Tree Rotation" mod
                             long setofBits = BitConverter.DoubleToInt64Bits(position.magnitude);
@@ -74,6 +76,7 @@ namespace Shicho.Patcher.TreeInstancePatch
 
                     materialBlock.Clear();
                     materialBlock.SetColor(instance.ID_Color, color);
+                    materialBlock.SetVector(instance.ID_ObjectIndex, objectIndex);
                     instance.m_drawCallData.m_defaultCalls++;
 
                     Graphics.DrawMesh(info.m_mesh, matrix, info.m_material, info.m_prefabDataLayer, null, 0, materialBlock);
@@ -89,6 +92,7 @@ namespace Shicho.Patcher.TreeInstancePatch
 
                     info.m_lodLocations[info.m_lodCount] = new Vector4(position.x, position.y, position.z, scale);
                     info.m_lodColors[info.m_lodCount] = color.linear;
+                    info.m_lodObjectIndices[info.m_lodCount] = objectIndex;
                     info.m_lodMin = Vector3.Min(info.m_lodMin, position);
                     info.m_lodMax = Vector3.Max(info.m_lodMax, position);
 
@@ -114,6 +118,7 @@ namespace Shicho.Patcher.TreeInstancePatch
                     typeof(Vector3),
                     typeof(float),
                     typeof(float),
+                    typeof(Vector4),
                     typeof(int).MakeByRefType(),
                     typeof(int).MakeByRefType(),
                     typeof(Vector3),
@@ -126,7 +131,7 @@ namespace Shicho.Patcher.TreeInstancePatch
             );
         }
 
-        static bool Prefix(TreeInfo info, Vector3 position, float scale, float brightness, ref int vertexIndex, ref int triangleIndex, Vector3 groupPosition, RenderGroup.MeshData data, ref Vector3 min, ref Vector3 max, ref float maxRenderDistance, ref float maxInstanceDistance)
+        static bool Prefix(TreeInfo info, Vector3 position, float scale, float brightness, Vector4 objectIndex, ref int vertexIndex, ref int triangleIndex, Vector3 groupPosition, RenderGroup.MeshData data, ref Vector3 min, ref Vector3 max, ref float maxRenderDistance, ref float maxInstanceDistance)
         {
             float y = info.m_generatedInfo.m_size.y * scale;
             float num = Mathf.Max(info.m_generatedInfo.m_size.x, info.m_generatedInfo.m_size.z) * scale * 0.5f;
@@ -151,21 +156,25 @@ namespace Shicho.Patcher.TreeInstancePatch
             position -= groupPosition;
             position.y += info.m_generatedInfo.m_center.y * scale;
             data.m_vertices[vertexIndex] = position + new Vector3(info.m_renderUv0B.x * scale, info.m_renderUv0B.y * scale, 0f);
+            data.m_normals[vertexIndex] = objectIndex;
             data.m_uvs[vertexIndex] = info.m_renderUv0;
             data.m_uvs2[vertexIndex] = info.m_renderUv0B * scale;
             data.m_colors[vertexIndex] = color;
             vertexIndex++;
             data.m_vertices[vertexIndex] = position + new Vector3(info.m_renderUv1B.x * scale, info.m_renderUv1B.y * scale, 0f);
+            data.m_normals[vertexIndex] = objectIndex;
             data.m_uvs[vertexIndex] = info.m_renderUv1;
             data.m_uvs2[vertexIndex] = info.m_renderUv1B * scale;
             data.m_colors[vertexIndex] = color;
             vertexIndex++;
             data.m_vertices[vertexIndex] = position + new Vector3(info.m_renderUv2B.x * scale, info.m_renderUv2B.y * scale, 0f);
+            data.m_normals[vertexIndex] = objectIndex;
             data.m_uvs[vertexIndex] = info.m_renderUv2;
             data.m_uvs2[vertexIndex] = info.m_renderUv2B * scale;
             data.m_colors[vertexIndex] = color;
             vertexIndex++;
             data.m_vertices[vertexIndex] = position + new Vector3(info.m_renderUv3B.x * scale, info.m_renderUv3B.y * scale, 0f);
+            data.m_normals[vertexIndex] = objectIndex;
             data.m_uvs[vertexIndex] = info.m_renderUv3;
             data.m_uvs2[vertexIndex] = info.m_renderUv3B * scale;
             data.m_colors[vertexIndex] = color;
